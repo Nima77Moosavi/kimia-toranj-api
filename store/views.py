b0b67b52321e8dfd8c325cf75b2149b7fdcecc7a
Nima77Moosavi <nima77moosavi@gmail.com>
2025-03-06 11:46:43 +0330

    Update views.py

--- store/views.py
+++ store/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .serializers import CollectionSerializer, ProductSerializer
 from .models import Collection, Product
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from django_filters import rest_framework as filters
 from .filters import ProductFilter
 
@@ -9,6 +11,13 @@ from .filters import ProductFilter
 class CollectionViewSet(viewsets.ModelViewSet):
     queryset = Collection.objects.all()
     serializer_class = CollectionSerializer
+    
+    @action(methods=['get'], detail=True)
+    def products(self, request, pk=None):
+        collection = self.get_object()
+        products = Product.objects.filter(collection=collection)
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
 
 
 class ProductViewSet(viewsets.ModelViewSet):
